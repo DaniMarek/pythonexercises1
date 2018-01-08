8598c64c749062fcc276b711e085b4587f90296c
DaniMarek <DaniMarek>
2018-01-08 15:36:36 -0600

    initial commit

---
+++ musuav.py
@@ -0,0 +1,23 @@
+# multiples
+# for i in range(1,1000):
+# 	if i % 2!=0:
+# 		print i
+
+# for x in range(5,1000001):
+# 	if x % 5==0:
+# 		print x
+
+# sum list
+a=[1,2,5,10,255,3]
+sum=0
+for element in a:
+	sum+=element
+print sum	
+
+# avg list
+a=[1,2,5,10,255,3]
+sum=0
+for element in a:
+	sum+=element
+	avg=sum/len(a)
+print avg
